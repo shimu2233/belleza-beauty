bc91ea3f39d24a0949db7da71390c8bcdd8a36ef
Haruto Suzuki <s49569292@gmail.com>
2026-05-11 02:08:57 +0900

    ハンバーガーメニューとアニメーションの追加

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -123,7 +123,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "belleza" / "static",
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
